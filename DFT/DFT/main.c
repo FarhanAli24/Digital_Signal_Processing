26438eb1018efc73b4526d8080197944767b2666
FarhanAli24 <146472794+FarhanAli24@users.noreply.github.com>
2024-08-01 17:11:46 -0400

    DFT Update

--- DFT/DFT/main.c
+++ DFT/DFT/main.c
@@ -12,18 +12,22 @@
 extern double InputSignal_f32_1kHz_15kHz[SIGNAL_LENGTH];
 double output_real[SIGNAL_LENGTH/2];
 double output_imaginary[SIGNAL_LENGTH/2];
+double output_mag[SIGNAL_LENGTH/2];
 
 void clac_sig_dft(double *sig_src_arr, double *sig_real_arr, double *sig_imaginary_arr, int sig_length);
+void gft_mag(double *sig_dest_mag_arr);
 
 int main(int argc, const char * argv[]) {
     
-    FILE *fileIn, *fileOutReal, *fileOutImag;
+    FILE *fileIn, *fileOutReal, *fileOutImag, *fileOutMag;
     
     clac_sig_dft((double*) &InputSignal_f32_1kHz_15kHz[0], (double*) &output_real[0], (double*) &output_imaginary[0], (int) SIGNAL_LENGTH);
+    gft_mag((double*) output_mag);
     
     fileIn = fopen("input_signal.dat", "w");
     fileOutReal = fopen("output_real.dat", "w");
     fileOutImag = fopen("output_imagninary.dat", "w");
+    fileOutMag = fopen("output_magnitude.dat", "w");
     
     for (int i = 0; i < SIGNAL_LENGTH; i++) {
         
@@ -33,10 +37,13 @@ int main(int argc, const char * argv[]) {
     for (int i = 0; i < (SIGNAL_LENGTH/2); i++) {
         fprintf(fileOutReal, "\n%f", output_real[i]);
         fprintf(fileOutImag, "\n%f", output_imaginary[i]);
+        fprintf(fileOutMag, "\n%f", output_mag[i]);
     }
+    
     fclose(fileIn);
     fclose(fileOutReal);
     fclose(fileOutImag);
+    fclose(fileOutMag);
     
     return 0;
 }
@@ -59,4 +66,10 @@ void clac_sig_dft(double *sig_src_arr, double *sig_dest_real_arr, double *sig_de
     }
     
     
+}
+
+void gft_mag(double *sig_dest_mag_arr){
+    for(int i=0; i < SIGNAL_LENGTH; i++){
+        sig_dest_mag_arr[i] = sig_dest_mag_arr[i] + sqrt(pow(output_real[i], 2) + pow(output_imaginary[i], 2));
+    }
 }
